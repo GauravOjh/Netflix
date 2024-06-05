378c48d7cb29f21bd35f47e63c8a848e32d5ef03
Gaurav <gauravojha1111@gmail.com>
2024-06-05 23:30:24 +0530

    embedyt

--- Netflix/src/app/core/components/banner/banner/banner.component.ts
+++ Netflix/src/app/core/components/banner/banner/banner.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges, inject } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-banner',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
+  @Input({required: true}) bannerTitle = '';
+  @Input() bannerOverview = '';
+  @Input() key = 'r_pUE7OcN8w';
+  private sanitizer = inject(DomSanitizer)
+  videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0`)
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['key']){
+      this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0`);
+    }
+  }
 }
